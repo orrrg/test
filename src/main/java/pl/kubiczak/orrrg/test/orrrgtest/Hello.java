4ca8ad9abffb051559de41834e2a8d32a94a41d5
Wiiitek <kubiczak.jakub@gmail.com>
2017-03-20 23:31:20 +0000

    some changes added to a branch

--- src/main/java/pl/kubiczak/orrrg/test/orrrgtest/Hello.java
+++ src/main/java/pl/kubiczak/orrrg/test/orrrgtest/Hello.java
@@ -3,6 +3,11 @@ package pl.kubiczak.orrrg.test.orrrgtest;
 public class Hello {
 
   String getMessage(String name) {
+
+    if (true) {
+      String msg = "this should be picked-up by SonarQube";
+    }
+
     return "Hello " + name + "!";
   }
 }
