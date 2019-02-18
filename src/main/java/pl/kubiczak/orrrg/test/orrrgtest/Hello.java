2a8cf171baa563d3603965b7a7c5d1d150042460
Wiiitek <wiiitek@users.noreply.github.com>
2019-02-18 22:31:23 +0100

    removing not needed code (#4)

--- src/main/java/pl/kubiczak/orrrg/test/orrrgtest/Hello.java
+++ src/main/java/pl/kubiczak/orrrg/test/orrrgtest/Hello.java
@@ -1,13 +1,8 @@
 package pl.kubiczak.orrrg.test.orrrgtest;
 
-public class Hello {
+class Hello {
 
   String getMessage(String name) {
-
-    if (true) {
-      String msg = "this should be picked-up by SonarQube";
-    }
-
     return "Hello " + name + "!";
   }
 }
